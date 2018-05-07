0dee764d8e7cfed7be46348ab7984c449aa518f3
David Knopp <daknopp@gmail.com>
2018-05-06 23:38:36 -0700

    Added Range helper

---
+++ Assets/Scripts/Atlas/Runtime/Utils/Range.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Atlas
+{
+    [Serializable]
+    public struct Range
+    {
+        public float m_minValue;
+        public float m_maxValue;
+
+        public float GetRandom()
+        {
+            return UnityEngine.Random.Range( m_minValue, m_maxValue );
+        }
+
+        public bool Contains( float value )
+        {
+            return value >= m_minValue && value <= m_maxValue;
+        }
+    }
+}

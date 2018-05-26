0ccc7a38e97302b535c8f984c336235fa457850b
David Knopp <daknopp@gmail.com>
2018-05-25 22:28:29 -0700

    Added color scopes

---
+++ Assets/Scripts/Atlas/Runtime/Utils/ColorScope/GLColorScope.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Atlas
+{
+    public class GLColorScope : IDisposable
+    {
+        public GLColorScope( Color color )
+        {
+            m_prevColor = GUI.color;
+            GL.Color( color );
+        }
+
+        public void Dispose()
+        {
+            if ( !m_disposed )
+            {
+                GL.Color( m_prevColor );
+                m_disposed = true;
+            }
+        }
+
+        private Color m_prevColor;
+        private bool m_disposed;
+    }
+}
---
+++ Assets/Scripts/Atlas/Runtime/Utils/ColorScope/GUIColorScope.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Atlas
+{
+    public class GUIColorScope : IDisposable
+    {
+        public GUIColorScope( Color color )
+        {
+            m_prevColor = GUI.color;
+            GUI.color = color;
+        }
+
+        public void Dispose()
+        {
+            if ( !m_disposed )
+            {
+                GUI.color = m_prevColor;
+                m_disposed = true;
+            }
+        }
+
+        private Color m_prevColor;
+        private bool m_disposed;
+    }
+}

db8efc9b5b2dfa50eeb73ab6718eafc4b8195859
David Knopp <daknopp@gmail.com>
2018-12-12 20:58:15 -0800

    Added PreventEditInInspector property

---
+++ Assets/Scripts/Atlas/Editor/Utils/Attributes/PreventEditInInspectorPropertyDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Atlas
+{
+    [CustomPropertyDrawer( typeof( PreventEditInInspectorAttribute ) )]
+    public sealed class PreventEditInInspectorPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField( position, property, label );
+            GUI.enabled = true;
+        }
+    }
+}

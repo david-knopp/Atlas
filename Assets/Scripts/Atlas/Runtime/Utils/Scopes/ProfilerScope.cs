e5151e5128cde049f4c2ff787f68b7882647f260
David Knopp <daknopp@gmail.com>
2018-12-27 11:31:12 -0800

    Added ProfilerScope object

---
+++ Assets/Scripts/Atlas/Runtime/Utils/Scopes/ProfilerScope.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+using Object = UnityEngine.Object;
+
+namespace Atlas
+{
+    public class ProfilerScope : IDisposable
+    {
+        public ProfilerScope( string name )
+        {
+            Profiler.BeginSample( name );
+        }
+
+        public ProfilerScope( string name, Object targetObject )
+        {
+            Profiler.BeginSample( name, targetObject );
+        }
+
+        public void Dispose()
+        {
+            if ( !m_disposed )
+            {
+                Profiler.EndSample();
+                m_disposed = true;
+            }
+        }
+
+        private Color m_prevColor;
+        private bool m_disposed;
+    }
+}

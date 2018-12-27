808ed5f31a02ad73802f8aba067dcfcd23c6c203
David Knopp <daknopp@gmail.com>
2018-12-27 12:55:05 -0800

    Addred [ScenePath] attribute

---
+++ Assets/Scripts/Atlas/Runtime/Utils/Attributes/ScenePathAttribute.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System;
+
+namespace Atlas
+{
+    [AttributeUsage( AttributeTargets.Field, AllowMultiple = false )]
+    public class ScenePathAttribute : PropertyAttribute
+    {
+    }
+}

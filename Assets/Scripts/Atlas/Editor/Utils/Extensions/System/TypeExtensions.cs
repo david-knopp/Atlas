e82b2e03baf20f0f93b8ebffd7f4ef0aa59970ac
David Knopp <daknopp@gmail.com>
2018-12-28 12:47:47 -0800

    Added extension for getting "primitive" name from Types (string instead of System.String)

---
+++ Assets/Scripts/Atlas/Editor/Utils/Extensions/System/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom;
+
+namespace Atlas
+{
+    public static class TypeExtensions
+    {
+        public static string GetPrimitiveName( this Type type )
+        {
+            var compiler = new CSharpCodeProvider();
+            var typeReference = new CodeTypeReference( type );
+
+            return compiler.GetTypeOutput( typeReference );
+        }
+    }
+}

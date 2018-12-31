207d52f38d343649fa9e660223eedc69f9c955c2
David Knopp <daknopp@gmail.com>
2018-12-31 00:16:49 -0800

    Rearranged test folder, added attribute test class

---
+++ Assets/Scripts/Test/Runtime/Demo/Utils/Attributes/AttributesTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Atlas.Test
+{
+    public sealed class AttributesTest : MonoBehaviour
+    {
+        [SerializeField, ScenePath] private string m_scenePath;
+        [SerializeField, MaxValue( 1.0f )] private float m_floatMax;
+        [SerializeField, MinValue( 0.0f )] private float m_floatMin;
+        [SerializeField, MaxValue( 1 )] private int m_intMax;
+        [SerializeField, MinValue( 0 )] private int m_intMin;
+        [SerializeField, PreventEditInInspector] private bool m_notEditable;
+    }
+}

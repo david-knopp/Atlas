a555ecaeb1f8059d2872c6012f8456e79d7c54de
David Knopp <daknopp@gmail.com>
2019-01-05 16:37:27 -0800

    Removed unused code

--- Assets/Scripts/Test/Runtime/Demo/Utils/SimpleOrbit.cs
+++ Assets/Scripts/Test/Runtime/Demo/Utils/SimpleOrbit.cs
@@ -7,13 +7,6 @@ namespace Atlas.Test
         [SerializeField] private float m_orbitSpeed;
         [SerializeField] private Vector3 m_pivotPoint;
 
-        private float m_radius;
-
-        private void Start()
-        {
-            m_radius = Mathf.Max( Vector3.Distance( transform.position, m_pivotPoint ), 0.1f );
-        }
-
         private void Update()
         {
             transform.RotateAround( m_pivotPoint, Vector3.up, m_orbitSpeed * Time.deltaTime );

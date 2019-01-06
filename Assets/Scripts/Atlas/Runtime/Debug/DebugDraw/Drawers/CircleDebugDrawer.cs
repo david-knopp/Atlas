4c4755e16396ca53a4dce7ba1e0afe94d4a4fec1
David Knopp <daknopp@gmail.com>
2019-01-05 16:50:18 -0800

    Updated transform stategy for debug draw circles and crosses

--- Assets/Scripts/Atlas/Runtime/Debug/DebugDraw/Drawers/CircleDebugDrawer.cs
+++ Assets/Scripts/Atlas/Runtime/Debug/DebugDraw/Drawers/CircleDebugDrawer.cs
@@ -4,9 +4,8 @@ namespace Atlas
 {
     public struct CircleDebugDrawer : IDebugDrawer
     {
-        public CircleDebugDrawer( Vector3 centerPos, float radius, Color color, int numSegments )
+        public CircleDebugDrawer( float radius, Color color, int numSegments )
         {
-            m_centerPos = centerPos;
             m_radius = radius;
             m_numSegments = Mathf.Max( numSegments, 1 );
             Color = color;
@@ -28,30 +27,20 @@ namespace Atlas
 
         public void Draw()
         {
-            Vector3 up = Vector3.up;
-            Vector3 right = Vector3.right;
-
-            if ( Camera.main != null )
-            {
-                up = Camera.main.transform.up;
-                right = Camera.main.transform.right;
-            }
-
             GL.Begin( GL.LINE_STRIP );
             GL.Color( Color );
 
             for ( int i = 0; i < m_numSegments + 1; ++i )
             {
                 float angle = ( i % m_numSegments ) * ( 2.0f * Mathf.PI / m_numSegments );
-                Vector3 offset = right * Mathf.Cos( angle ) * m_radius +
-                                 up * Mathf.Sin( angle ) * m_radius;
-                GL.Vertex( m_centerPos + offset );
+                Vector2 vertPosition = new Vector2( Mathf.Cos( angle ) * m_radius, 
+                                              Mathf.Sin( angle ) * m_radius );
+                GL.Vertex( vertPosition );
             }
 
             GL.End();
         }
-
-        private Vector3 m_centerPos;
+        
         private float m_radius;
         private int m_numSegments;
     }

7414bfd21fa9f6523ab85af563662c0ee910ca50
David Knopp <daknopp@gmail.com>
2019-01-19 23:29:00 -0800

    Cleaned up directory structure, started adding samples and documentation

---
+++ Assets/Examples/Scripts/Runtime/Example_MaxValueAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Atlas.Examples
+{
+    public sealed class Example_MaxValueAttribute : MonoBehaviour
+    {
+        [SerializeField, MaxValue( 10.0f )] private float m_value;
+    }
+}

bfc58ea9b2279fe7b2889c1a788e6faf19c538ee
David Knopp <daknopp@gmail.com>
2019-02-04 21:24:27 -0800

    Added support for file paths, in addition to directory paths, when using the PathAttribute

--- Assets/Examples/Scripts/Runtime/Utils/Attributes/Example_PathAttribute.cs
+++ Assets/Examples/Scripts/Runtime/Utils/Attributes/Example_PathAttribute.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
 
-using PathType = Atlas.PathAttribute.PathType;
+using Path = Atlas.PathAttribute.Path;
+using Relativity = Atlas.PathAttribute.Relativity;
 
 namespace Atlas.Examples
 {
     public sealed class Example_PathAttribute : MonoBehaviour
     {
-        [SerializeField, Path( PathType.ProjectRelative )] private string m_path;
+        [Header( "Absolute" )]
+        [SerializeField, Path( Path.File, Relativity.ProjectRelative )]
+        private string m_filePathAbsolute;
+
+        [SerializeField, Path( Path.Directory, Relativity.ProjectRelative )]
+        private string m_directoryPathAbsolute;
+
+        [Header( "Assets-relative" )]
+        [SerializeField, Path( Path.File, Relativity.AssetsRelative )]
+        private string m_filePathAssets;
+
+        [SerializeField, Path( Path.Directory, Relativity.AssetsRelative )]
+        private string m_directoryPathAssets;
+
+        [Header( "Project-relative" )]
+        [SerializeField, Path( Path.File, Relativity.ProjectRelative )]
+        private string m_filePathProject;
+
+        [SerializeField, Path( Path.Directory, Relativity.ProjectRelative )]
+        private string m_directoryPathProject;
     }
 }

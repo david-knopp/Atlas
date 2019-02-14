825c341636be2ce80c4783a85cedd450e0cfd60a
David Knopp <daknopp@gmail.com>
2019-02-13 21:54:37 -0800

    Cleaned up command classes, and added example files

---
+++ Assets/Examples/Scripts/Runtime/Framework/Command/Example_Command.cs
@@ -0,0 +1,27 @@
+﻿namespace Atlas.Examples
+{
+    public sealed class Example_Command
+    {
+        // command declaration
+        public struct ExampleCommand : ICommand<float>
+        {
+            public void Execute( float value )
+            {
+                UnityEngine.Debug.LogFormat( "Command executed with value of {0}", value );
+            }
+        }
+
+        public void OnInjectDependencies( Signal<float> exampleSignal )
+        {
+            // instantiate a command object
+            ExampleCommand command = new ExampleCommand();
+
+            // register a command as a listener of the signal
+            exampleSignal.AddCommand( command );
+
+            // ...
+            // further calls to exampleSignal.Dispatch( float value ) will also
+            // invoke command.Execute( float value )
+        }
+    }
+}
---
+++ Assets/Examples/Scripts/Runtime/Framework/Signal/Example_Signal.cs
@@ -0,0 +1,35 @@
+﻿namespace Atlas.Examples
+{
+    public sealed class Example_Signal
+    {
+        public enum CharacterState
+        {
+            Alive,
+            Dead
+        }
+
+        // signal declaration
+        public class CharacterStateChangedSignal : Signal<CharacterState>
+        {
+        }
+
+        // called externally to provide relevant dependencies
+        public void OnInjectDependencies( CharacterStateChangedSignal signal )
+        {
+            m_signal = signal;
+        }
+
+        public void TakeDamage( int damage )
+        {
+            m_curHealth -= damage;
+            if ( m_curHealth <= 0 )
+            {
+                // dispatches state change to all listeners
+                m_signal.Dispatch( CharacterState.Dead );
+            }
+        }
+
+        private CharacterStateChangedSignal m_signal;
+        private int m_curHealth;
+    }
+}

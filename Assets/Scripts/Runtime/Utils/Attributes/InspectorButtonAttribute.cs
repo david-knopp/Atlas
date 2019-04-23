7c4040b0754ce6019e5491a58761a31c05cdbbb0
David Knopp <daknopp@gmail.com>
2019-04-22 21:34:23 -0700

    Added inspector button attribute and MonoBehaviour editor

---
+++ Assets/Scripts/Runtime/Utils/Attributes/InspectorButtonAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Atlas
+{
+    /// <summary>
+    /// Automatically adds a button in the inspector that executes the marked function on click.
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Method, AllowMultiple = false )]
+    public sealed class InspectorButtonAttribute : PropertyAttribute
+    {
+    }
+}

c54f4d7564c4eb30771132bdbffb049ee57bece8
David Knopp <daknopp@gmail.com>
2019-04-23 19:55:37 -0700

    Added initial prefab pooling work

---
+++ Assets/Scripts/Runtime/Pooling/IPoolable.cs
@@ -0,0 +1,15 @@
+﻿namespace Atlas
+{
+    public interface IPoolable
+    {
+        /// <summary>
+        /// Called when the object is instantiated from its pool
+        /// </summary>
+        void OnPoolInstantiate();
+
+        /// <summary>
+        /// Called when the object is destroyed and returned to its pool
+        /// </summary>
+        void OnPoolDestroy();
+    }
+}

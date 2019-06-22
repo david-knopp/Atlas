e64a2ad94cc9b56e2762641c648b0d446de4e4b1
David Knopp <daknopp@gmail.com>
2019-06-22 16:49:57 -0700

    Added more gui color scopes

--- Assets/Scripts/Runtime/Utils/Scopes/GUIBackgroundColorScope.cs
+++ Assets/Scripts/Runtime/Utils/Scopes/GUIBackgroundColorScope.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 namespace Atlas
 {
     /// <summary>
-    /// A handy scope variable that sets <see cref="GUI.color"/> to the given color, 
+    /// A handy scope variable that sets <see cref="GUI.backgroundColor"/> to the given color, 
     /// and automatically resets it to its original color when the scope ends
     /// </summary>
     /// <seealso cref="ProfilerScope"/>
-    public struct GUIColorScope : IDisposable
+    public struct GUIBackgroundColorScope : IDisposable
     {
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="color">The target color</param>
         /// <param name="shouldSetColor">Optional conditional for bypassing color assignment</param>
-        public GUIColorScope( Color color, bool shouldSetColor = true )
+        public GUIBackgroundColorScope( Color color, bool shouldSetColor = true )
         {
             m_disposed = false;
-            m_prevColor = GUI.color;
+            m_prevColor = GUI.backgroundColor;
 
             if ( shouldSetColor )
             {
-                GUI.color = color; 
+                GUI.backgroundColor = color;
             }
         }
 
@@ -33,7 +33,7 @@ namespace Atlas
         {
             if ( !m_disposed )
             {
-                GUI.color = m_prevColor;
+                GUI.backgroundColor = m_prevColor;
                 m_disposed = true;
             }
         }

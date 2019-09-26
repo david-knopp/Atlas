2ec1f272398a48a3f4fe51973831e2f4ee495c87
David Knopp <daknopp@gmail.com>
2019-09-25 19:33:33 -0700

    Added List shuffle extension

---
+++ Assets/Scripts/Runtime/Utils/Extensions/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Atlas
+{
+    public static class ListExtensions
+    {
+
+        public static void Shuffle<T>( this IList<T> list )
+        {
+            for ( int i = list.Count - 1; i >= 0; i-- )
+            {
+                int swapIndex = Random.Range( 0, i + 1 );
+                T temp = list[swapIndex];
+                list[swapIndex] = list[i];
+                list[i] = temp;
+            }
+        }
+    }
+}

f7bbe23e235a3cd093f7b635014898a7bcdd002e
David Knopp <daknopp@gmail.com>
2019-10-05 21:25:08 -0700

    Added generic Swap utility

---
+++ Assets/Scripts/Runtime/Utility/Utility.cs
@@ -0,0 +1,12 @@
+﻿namespace Atlas
+{
+    public static class Utility
+    {
+        public static void Swap<T>( ref T lhs, ref T rhs )
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+}

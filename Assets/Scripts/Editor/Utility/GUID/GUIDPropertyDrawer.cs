ccef3e7cf31f5b63bd49049cacb700783856fb33
David Knopp <daknopp@gmail.com>
2019-12-27 16:44:45 -0800

    Added serializable GUID

---
+++ Assets/Scripts/Editor/Utility/GUID/GUIDPropertyDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Atlas
+{
+    [CustomPropertyDrawer( typeof ( GUID ) )]
+    public sealed class GUIDPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+        {
+            GUID guid = ( GUID )fieldInfo.GetValue( property.serializedObject.targetObject );
+
+            using ( new EditorGUI.DisabledScope( true ) )
+            {
+                EditorGUI.TextField( position, label, guid.Value.ToString() );
+            }
+        }
+    }
+}

962123b70c860a1a2714a54950587e28f2f059a3
David Knopp <daknopp@gmail.com>
2019-12-28 12:31:45 -0800

    Added attributes for preventing edit in play mode & edit mode

---
+++ Assets/Scripts/Editor/Utility/Attributes/PreventEditInEditModePropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Atlas
+{
+    /// <summary>
+    /// Handles inspector drawing for <see cref="PreventEditInEditModeAttribute"/>
+    /// </summary>
+    [CustomPropertyDrawer( typeof( PreventEditInEditModeAttribute ) )]
+    public sealed class PreventEditInEditModePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+        {
+            using ( new EditorGUI.DisabledScope( Application.isPlaying ) )
+            {
+                EditorGUI.PropertyField( position, property, label );
+            }
+        }
+    }
+}

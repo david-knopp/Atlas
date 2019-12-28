8b78a746f2d202a748bd47afd3ce367f66a25851
David Knopp <daknopp@gmail.com>
2019-12-28 12:31:45 -0800

    Added attributes for preventing edit in play mode & edit mode

---
+++ Assets/Scripts/Runtime/Utility/Attributes/PreventEditInEditModeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Atlas
+{
+    /// <summary>
+    /// Prevents the target field from being edited in the inspector window when in edit mode
+    /// (becomes editable after entering playmode)
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Field, AllowMultiple = false )]
+    public sealed class PreventEditInEditModeAttribute : PropertyAttribute
+    {
+    }
+}
---
+++ Assets/Scripts/Runtime/Utility/Attributes/PreventEditInPlayModeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Atlas
+{
+    /// <summary>
+    /// Prevents the target field from being edited in the inspector window when in play mode
+    /// (becomes editable after entering edit mode)
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Field, AllowMultiple = false )]
+    public sealed class PreventEditInPlayModeAttribute : PropertyAttribute
+    {
+    }
+}

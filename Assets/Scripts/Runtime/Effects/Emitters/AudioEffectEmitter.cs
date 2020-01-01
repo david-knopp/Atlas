4ee3c3ffd19442f0b4d73f75e1d51451c9a64860
David Knopp <daknopp@gmail.com>
2019-12-31 17:10:56 -0800

    Added Audio effect emitter

---
+++ Assets/Scripts/Runtime/Effects/Emitters/AudioEffectEmitter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Atlas
+{
+    [RequireComponent( typeof( AudioSource ) )]
+    public sealed class AudioEffectEmitter : EffectEmitterBase
+    {
+        #region public
+        /// <summary>
+        /// Starts playing the underlying audio source
+        /// </summary>
+        public override void Play()
+        {
+            base.Play();
+            m_audioSource.Play();
+        }
+
+        /// <summary>
+        /// Stops playing the audio source
+        /// </summary>
+        public override void Stop()
+        {
+            base.Stop();
+            m_audioSource.Stop();
+        }
+        #endregion public
+
+        #region protected
+        protected override bool IsFinished
+        {
+            get => m_audioSource.isPlaying == false;
+        } 
+        #endregion protected
+
+        #region private
+        private AudioSource m_audioSource;
+
+        private void Awake()
+        {
+            m_audioSource = GetComponent<AudioSource>();
+            m_audioSource.playOnAwake = false;
+        } 
+        #endregion private
+    }
+}

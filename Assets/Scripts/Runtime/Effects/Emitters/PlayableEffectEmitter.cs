5fbc0a3d70516e7b7ad520eca23154cebb5ffe6a
David Knopp <daknopp@gmail.com>
2019-12-31 17:25:08 -0800

    Added Playable effect emitter

---
+++ Assets/Scripts/Runtime/Effects/Emitters/PlayableEffectEmitter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Atlas
+{
+    /// <summary>
+    /// Handles playable playback for use with the effect system
+    /// </summary>
+    [RequireComponent( typeof( PlayableDirector ) )]
+    public sealed class PlayableEffectEmitter : EffectEmitterBase
+    {
+        #region public
+        public override void Play()
+        {
+            base.Play();
+            m_playable.Play();
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+            m_playable.Stop();
+        }
+        #endregion public
+
+        #region protected
+        protected override bool IsFinished => m_playable.state == PlayState.Paused;
+        #endregion protected
+
+        #region private
+        private PlayableDirector m_playable;
+
+        private void Awake()
+        {
+            m_playable = GetComponent<PlayableDirector>();
+            m_playable.playOnAwake = false;
+        } 
+        #endregion private
+    }
+}

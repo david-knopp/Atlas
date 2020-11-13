6ce18478cbd77ba33cacb92d97101dcf70437ffa
LapKnopp <daknopp@gmail.com>
2020-11-12 21:25:26 -0800

    Added ease property drawer to draw ease inspector on one line

---
+++ Assets/Scripts/Editor/Math/Easing/EasePropertyDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Atlas
+{
+    [CustomPropertyDrawer( typeof( Ease ) )]
+    public sealed class EasePropertyDrawer : PropertyDrawer
+    {
+        public override float GetPropertyHeight( SerializedProperty property, GUIContent label )
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public override void OnGUI( Rect position, SerializedProperty property, GUIContent label )
+        {
+            var typeProperty = property.FindPropertyRelative( "m_type" );
+            EditorGUI.PropertyField( position, typeProperty, label );
+        }
+    }
+}

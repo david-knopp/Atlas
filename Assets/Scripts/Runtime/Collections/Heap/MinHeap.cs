ac9447e784e122964b87d7392ff319a2eccc4112
LapKnopp <daknopp@gmail.com>
2021-01-30 14:51:57 -0800

    Added min & max heap

---
+++ Assets/Scripts/Runtime/Collections/Heap/MinHeap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atlas
+{
+    public sealed class MinHeap<T> : BinaryHeap<T>
+        where T : IComparable<T>
+    {
+        public MinHeap()
+            : this( capacity: 0 )
+        {
+        }
+
+        public MinHeap( int capacity )
+            : base( new Comparer(), capacity )
+        {
+        }
+
+        private sealed class Comparer : IComparer<T>
+        {
+            public int Compare( T x, T y )
+            {
+                return x.CompareTo( y );
+            }
+        }
+    }
+}

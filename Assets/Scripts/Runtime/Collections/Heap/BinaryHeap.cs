5fb839f5d2c7a517ddfc1a83883eabee1be05699
LapKnopp <daknopp@gmail.com>
2021-01-30 14:41:22 -0800

    Added binary heap

---
+++ Assets/Scripts/Runtime/Collections/Heap/BinaryHeap.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atlas
+{
+    public class BinaryHeap<T>
+    {
+        public BinaryHeap()
+            : this( Comparer<T>.Default )
+        {
+        }
+
+        public BinaryHeap( IComparer<T> comparer )
+            : this( comparer, 0 )
+        {
+        }
+
+        public BinaryHeap( IComparer<T> comparer, int capacity )
+        {
+            m_comparer = comparer;
+            m_heap = new List<T>( capacity );
+        }
+
+        public int Count { get; private set; }
+        public int Capacity => m_heap.Capacity;
+
+        public void Add( T item )
+        {
+            m_heap.Add( item );
+            BubbleUp( Count++ );
+        }
+
+        public T Peek()
+        {
+            if ( Count == 0 )
+            {
+                throw new InvalidOperationException();
+            }
+
+            return m_heap[0];
+        }
+
+        public T Pop()
+        {
+            if ( Count == 0 )
+            {
+                throw new InvalidOperationException();
+            }
+
+            T top = m_heap[0];
+
+            Swap( 0, --Count );
+            m_heap.RemoveAt( Count );
+            BubbleDown( 0 );
+
+            return top;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            m_heap.Clear();
+        }
+
+        private IComparer<T> m_comparer;
+        private readonly List<T> m_heap;
+
+        private int GetParentIndex( int index )
+        {
+            return ( index - 1 ) / 2;
+        }
+
+        private int GetLeftChildIndex( int index )
+        {
+            return ( 2 * index ) + 1;
+        }
+
+        private int GetRightChildIndex( int index )
+        {
+            return ( 2 * index ) + 2;
+        }
+
+        private void BubbleUp( int index )
+        {
+            while ( index > 0 )
+            {
+                int parentIndex = GetParentIndex( index );
+
+                if ( m_comparer.Compare( m_heap[index], m_heap[parentIndex] ) >= 0 )
+                {
+                    break;
+                }
+
+                Swap( index, parentIndex );
+                index = parentIndex;
+            }
+        }
+
+        private void BubbleDown( int index )
+        {
+            while ( index < m_heap.Count )
+            {
+                int leftChildIndex = GetLeftChildIndex( index );
+                int rightChildIndex = GetRightChildIndex( index );
+
+                if ( leftChildIndex >= m_heap.Count )
+                {
+                    return;
+                }
+                if ( rightChildIndex >= m_heap.Count ||
+                     m_comparer.Compare( m_heap[leftChildIndex], m_heap[rightChildIndex] ) <= 0 )
+                {
+                    if ( m_comparer.Compare( m_heap[leftChildIndex], m_heap[index] ) >= 0 )
+                    {
+                        return;
+                    }
+
+                    Swap( leftChildIndex, index );
+                    index = leftChildIndex;
+                }
+                else
+                {
+                    if ( m_comparer.Compare( m_heap[rightChildIndex], m_heap[index] ) >= 0 )
+                    {
+                        return;
+                    }
+
+                    Swap( rightChildIndex, index );
+                    index = rightChildIndex;
+                }
+            }
+        }
+
+        private void Swap( int leftIndex, int rightIndex )
+        {
+            T temp = m_heap[leftIndex];
+            m_heap[leftIndex] = m_heap[rightIndex];
+            m_heap[rightIndex] = temp;
+        }
+    }
+}
---
+++ Assets/Test/Scripts/Editor/Unit/Collections/BinaryHeapTests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Atlas.Test
+{
+    public sealed class BinaryHeapTests
+    {
+        public sealed class LessIntComparer : IComparer<int>
+        {
+            public int Compare( int x, int y )
+            {
+                return x.CompareTo( y );
+            }
+        }
+
+        public sealed class GreaterIntComparer : IComparer<int>
+        {
+            public int Compare( int x, int y )
+            {
+                return y.CompareTo( x );
+            }
+        }
+
+        [Test]
+        [TestCase( 1 )]
+        [TestCase( 10 )]
+        [TestCase( 1000 )]
+        public void Add( int insertCount )
+        {
+            BinaryHeap<int> heap = new BinaryHeap<int>();
+
+            for ( int i = 0; i < insertCount; i++ )
+            {
+                heap.Add( i );
+            }
+
+            Assert.That( heap.Count == insertCount );
+        }
+
+        [Test]
+        [TestCase( 1 )]
+        [TestCase( 10 )]
+        public void PeekMin( int insertCount )
+        {
+            BinaryHeap<int> heap = new BinaryHeap<int>( new LessIntComparer() );
+
+            for ( int i = 0; i < insertCount; i++ )
+            {
+                heap.Add( i );
+            }
+
+            Assert.That( heap.Peek() == 0 );
+        }
+
+        [Test]
+        [TestCase( 1 )]
+        [TestCase( 10 )]
+        public void PeekMax( int insertCount )
+        {
+            BinaryHeap<int> heap = new BinaryHeap<int>( new GreaterIntComparer() );
+
+            for ( int i = 0; i < insertCount; i++ )
+            {
+                heap.Add( i );
+            }
+
+            Assert.That( heap.Peek() == insertCount - 1 );
+        }
+
+        [Test]
+        [TestCase( new int[] { 0, 1, 2 } )]
+        [TestCase( new int[] { 5, 1, -2, 18, 3, 9 } )]
+        [TestCase( new int[] { 14, 8, 1, 3, 1, 16, 45 } )]
+        public void PopValue( int[] values )
+        {
+            BinaryHeap<int> heap = new BinaryHeap<int>();
+
+            foreach ( int value in values )
+            {
+                heap.Add( value );
+            }
+
+            Array.Sort( values );
+
+            for ( int i = 0; i < values.Length; i++ )
+            {
+                int value = heap.Pop();
+                Assert.That( value == values[i] );
+            }
+        }
+
+        [Test]
+        [TestCase( 1 )]
+        [TestCase( 10 )]
+        public void PopSize( int insertCount )
+        {
+            BinaryHeap<int> heap = new BinaryHeap<int>();
+
+            for ( int i = 0; i < insertCount; i++ )
+            {
+                heap.Add( i );
+            }
+
+            heap.Pop();
+
+            Assert.That( heap.Count == insertCount - 1 );
+        }
+
+        [Test]
+        [TestCase( 0 )]
+        [TestCase( 10 )]
+        public void Clear( int insertCount )
+        {
+            BinaryHeap<int> heap = new BinaryHeap<int>();
+
+            for ( int i = 0; i < insertCount; i++ )
+            {
+                heap.Add( i );
+            }
+
+            heap.Clear();
+
+            Assert.That( heap.Count == 0 );
+        }
+    }
+}

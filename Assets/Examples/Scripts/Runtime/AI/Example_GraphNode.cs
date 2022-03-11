328e9ecb77edf52acdc47122c3c922b755d7e33b
David Knopp <daknopp@gmail.com>
2022-03-10 21:53:33 -0800

    Added quick A* test

---
+++ Assets/Examples/Scripts/Runtime/AI/Example_GraphNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Atlas.Examples
+{
+    public sealed class Example_GraphNode : MonoBehaviour, IGraphNode
+    {
+        [field: SerializeField]
+        public int ID { get; private set;  }
+
+        public Vector3 Position => transform.position;
+
+        public IReadOnlyList<Example_GraphNode> ConnectedNodes => m_connectedNodes;
+
+        [SerializeField]
+        private List<Example_GraphNode> m_connectedNodes;
+    }
+}

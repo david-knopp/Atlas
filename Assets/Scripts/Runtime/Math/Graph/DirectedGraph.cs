33ea5548ed07fdc0a610d3141b301037668aa9e9
LapKnopp <daknopp@gmail.com>
2022-03-28 22:05:05 -0700

    Added connecton reversal helpers

--- Assets/Scripts/Runtime/Math/Graph/DirectedGraph.cs
+++ Assets/Scripts/Runtime/Math/Graph/DirectedGraph.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 namespace Atlas
 {
@@ -61,7 +59,7 @@ namespace Atlas
         public bool TryGetNode( int nodeID, out TNode node )
         {
             return m_nodes.TryGetValue( nodeID, out node );
-        } 
+        }
 
         #endregion nodes
 
@@ -181,7 +179,7 @@ namespace Atlas
 
             connection = default;
             return false;
-        } 
+        }
 
         #endregion connections
 
--- Assets/Scripts/Runtime/Math/Graph/GraphConnection.cs
+++ Assets/Scripts/Runtime/Math/Graph/GraphConnection.cs
@@ -12,5 +12,17 @@
         public int FromNodeID { get; set; }
         public int ToNodeID { get; set; }
         public float Weight { get; set; }
+
+        public GraphConnection Reversed
+        {
+            get => new GraphConnection( ToNodeID, FromNodeID, Weight );
+        }
+
+        public void Reverse()
+        {
+            int originalFromID = FromNodeID;
+            FromNodeID = ToNodeID;
+            ToNodeID = originalFromID;
+        }
     }
 }

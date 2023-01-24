cc5e7f7b2054303af0ccebd31382bba51bfa056a
David Knopp <daknopp@gmail.com>
2023-01-23 20:45:58 -0800

    Added ActionList comments

--- Assets/Scripts/Runtime/Collections/ActionList/Actions/InterpolatedAction.cs
+++ Assets/Scripts/Runtime/Collections/ActionList/Actions/InterpolatedAction.cs
@@ -2,10 +2,26 @@
 
 namespace Atlas
 {
+    /// <summary>
+    /// A helper action that will execute for the given amount of time,
+    /// and caclulate an elapsed percentage from [0, 1]
+    /// </summary>
     public abstract class InterpolatedAction : IAction
     {
+        /// <summary>
+        /// Returns true when the given time has elapsed
+        /// </summary>
         public bool IsFinished => m_timer.HasElapsed( LengthSeconds );
+
+        /// <summary>
+        /// Whether or not this action will block the execution of subsequent actions
+        /// until it has finished running
+        /// </summary>
         public virtual bool IsBlocking { get; } = true;
+
+        /// <summary>
+        /// Whether or not the execution of this action is paused
+        /// </summary>
         public virtual bool IsPaused
         {
             get => m_timer.IsPaused;
@@ -32,9 +48,20 @@ namespace Atlas
             }
         }
 
+        /// <summary>
+        /// The amount of time this action takes to execute, in seconds
+        /// </summary>
         protected abstract float LengthSeconds { get; }
+
+        /// <summary>
+        /// The <see cref="TimeScale"/> this action will use to calculate time
+        /// </summary>
         protected virtual TimeScale TimeScale => TimeScale.Scaled;
 
+        /// <summary>
+        /// Called once per tick to update the action
+        /// </summary>
+        /// <param name="elapsedPercent">The action's current elapsed completion on [0,1]</param>
         protected abstract void OnTick( float elapsedPercent );
 
         private Timer m_timer;

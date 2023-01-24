f3f4ba401bc077559006b1c5f51e6ca140b7baf3
David Knopp <daknopp@gmail.com>
2023-01-23 20:31:43 -0800

    Added comments for heaps and priority queue

--- Assets/Scripts/Runtime/Collections/Heap/BinaryHeap.cs
+++ Assets/Scripts/Runtime/Collections/Heap/BinaryHeap.cs
@@ -3,38 +3,76 @@ using System.Collections.Generic;
 
 namespace Atlas
 {
+    /// <summary>
+    /// A heap data structure utilizing a binary tree, ensuring that root nodes are >= or <= its children,
+    /// determined by the provided <see cref="IComparer<typeparamref name="T"/>"/> object
+    /// </summary>
+    /// <typeparam name="T">Type of the heap's elements</typeparam>
     public class BinaryHeap<T>
     {
+        #region public
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public BinaryHeap()
             : this( Comparer<T>.Default )
         {
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="capacity">The number of elements the heap can hold before resizing</param>
         public BinaryHeap( int capacity )
             : this( Comparer<T>.Default, capacity )
         {
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="comparer">Comparer object to use when comparing elements</param>
         public BinaryHeap( IComparer<T> comparer )
             : this( comparer, 0 )
         {
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="comparer">Comparer object to use when comparing elements</param>
+        /// <param name="capacity">The number of elements the heap can hold before resizing</param>
         public BinaryHeap( IComparer<T> comparer, int capacity )
         {
             m_comparer = comparer;
             m_heap = new List<T>( capacity );
         }
 
+        /// <summary>
+        /// Number of elements in the heap
+        /// </summary>
         public int Count { get; private set; }
+
+        /// <summary>
+        /// The total number of elements the internal data structure can hold without resizing
+        /// </summary>
         public int Capacity => m_heap.Capacity;
 
+        /// <summary>
+        /// Adds the given item to the heap
+        /// </summary>
+        /// <param name="item">The item to add</param>
         public void Add( T item )
         {
             m_heap.Add( item );
             BubbleUp( Count++ );
         }
 
+        /// <summary>
+        /// Gets the item at the root of the heap
+        /// </summary>
+        /// <returns>The root item</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the heap is empty</exception>
         public T Peek()
         {
             if ( Count == 0 )
@@ -45,6 +83,11 @@ namespace Atlas
             return m_heap[0];
         }
 
+        /// <summary>
+        /// Removes the item at the root of the heap, and returns it
+        /// </summary>
+        /// <returns>The root item</returns>
+        /// <exception cref="InvalidOperationException">Thrown if the heap is emptyu</exception>
         public T Pop()
         {
             if ( Count == 0 )
@@ -61,12 +104,17 @@ namespace Atlas
             return top;
         }
 
+        /// <summary>
+        /// Removes all items from the heap
+        /// </summary>
         public void Clear()
         {
             Count = 0;
             m_heap.Clear();
-        }
+        } 
+        #endregion public
 
+        #region private
         private IComparer<T> m_comparer;
         private readonly List<T> m_heap;
 
@@ -141,6 +189,7 @@ namespace Atlas
             T temp = m_heap[leftIndex];
             m_heap[leftIndex] = m_heap[rightIndex];
             m_heap[rightIndex] = temp;
-        }
+        } 
+        #endregion private
     }
 }
--- Assets/Scripts/Runtime/Collections/PriorityQueue/PriorityQueue.cs
+++ Assets/Scripts/Runtime/Collections/PriorityQueue/PriorityQueue.cs
@@ -3,32 +3,62 @@ using System.Collections.Generic;
 
 namespace Atlas
 {
+    /// <summary>
+    /// A queue data structure that arranges elements based on their priority values
+    /// </summary>
+    /// <typeparam name="TItem">Type of contained elements</typeparam>
+    /// <typeparam name="TPriority">Type of each element's priority</typeparam>
     public sealed class PriorityQueue<TItem, TPriority>
         where TPriority : IComparable<TPriority>
     {
         #region public
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public PriorityQueue()
            : this( capacity: 0 )
         {
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="capacity">The number of elements the queue can hold before resizing</param>
         public PriorityQueue( int capacity )
             : this( Comparer<TPriority>.Default, capacity )
         {
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="priorityComparer">Comparer object to use when comparing element priorities</param>
+        /// <param name="capacity">The number of elements the queue can hold before resizing</param>
         public PriorityQueue( IComparer<TPriority> priorityComparer, int capacity )
         {
             m_heap = new BinaryHeap<Node>( new NodeComparer( priorityComparer ), capacity );
         }
 
+        /// <summary>
+        /// Number of elements in the queue
+        /// </summary>
         public int Count => m_heap.Count;
 
+        /// <summary>
+        /// Adds the given item to the queue
+        /// </summary>
+        /// <param name="item">The item to add</param>
+        /// <param name="priority">The priority of the added item</param>
         public void Enqueue( TItem item, TPriority priority )
         {
             m_heap.Add( new Node( item, priority ) );
         }
 
+        /// <summary>
+        /// Gets the next item from the queue with the best priority, and removes it
+        /// </summary>
+        /// <returns>The item with the highest priority</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the queue is empty</exception>
         public TItem Dequeue()
         {
             if ( m_heap.Count == 0 )
@@ -39,6 +69,11 @@ namespace Atlas
             return m_heap.Pop().Item;
         }
 
+        /// <summary>
+        /// Fetches the next item from the queue with the best priority without removing it
+        /// </summary>
+        /// <returns>The item with the highest priority</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the queue is empty</exception>
         public TItem Peek()
         {
             if ( m_heap.Count == 0 )
@@ -49,6 +84,9 @@ namespace Atlas
             return m_heap.Peek().Item;
         }
 
+        /// <summary>
+        /// Removes all elements from the queue
+        /// </summary>
         public void Clear()
         {
             m_heap.Clear();

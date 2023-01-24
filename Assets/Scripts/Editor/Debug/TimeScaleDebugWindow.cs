b34da2f95cd12ff8ffcffcf3f1b124ccbb10bf00
David Knopp <daknopp@gmail.com>
2023-01-23 18:00:54 -0800

    Added TimeScale debug window

---
+++ Assets/Scripts/Editor/Debug/TimeScaleDebugWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Atlas
+{
+    public sealed class TimeScaleDebugWindow : EditorWindow
+    {
+        [MenuItem( "Atlas/Debug/Time Scale Debug Window" )]
+        private static void Init()
+        {
+            var window = GetWindow<TimeScaleDebugWindow>( "Time Scale" );
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            using ( var changeCheck = new EditorGUI.ChangeCheckScope() )
+            {
+                float timeScale = EditorGUILayout.Slider( "Time Scale", Time.timeScale, 0f, 10f );
+
+                if ( changeCheck.changed )
+                {
+                    Time.timeScale = timeScale;
+                }
+            }
+
+            if ( GUILayout.Button( "Reset" ) )
+            {
+                Time.timeScale = 1.0f;
+            }
+
+            if ( GUILayout.Button( "Pause" ) )
+            {
+                Time.timeScale = 0.0f;
+            }
+        }
+    }
+}

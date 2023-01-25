72d9682a3a61e12487a10c18bf8c5b34ef8674d3
LapKnopp <daknopp@gmail.com>
2023-01-24 21:00:11 -0800

    Added comments for A*

--- Assets/Scripts/Runtime/AI/Pathfinding/AStar.cs
+++ Assets/Scripts/Runtime/AI/Pathfinding/AStar.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 
 namespace Atlas
 {
+    /// <summary>
+    /// Represents an A* path search algorithm on a <see cref="DirectedGraph{TNode, TConnection}"/>
+    /// </summary>
+    /// <typeparam name="TNode">Type of the graph's nodes</typeparam>
+    /// <typeparam name="TConnection">Type of connections between nodes</typeparam>
     public class AStar<TNode, TConnection>
         where TNode : IGraphNode
         where TConnection : struct, IGraphConnection
     {
         #region public
 
+        /// <summary>
+        /// Search result
+        /// </summary>
         public enum Result
         {
             Success = 0,
             Failure = 1
         }
 
+        /// <summary>
+        /// Heuristic definition used to estimate the cost of the
+        /// cheapest path between nodes
+        /// </summary>
+        /// <param name="fromNode">The start node to estimate the path from</param>
+        /// <param name="toNode">The goal node to estimate the path to</param>
+        /// <returns>Estimated cost of the path between the given nodes</returns>
         public delegate float Heuristic( TNode fromNode, TNode toNode );
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="graph">The graph on which to perform searches</param>
+        /// <param name="heuristic">The heuristic used to estimate path costs</param>
         public AStar( DirectedGraph<TNode, TConnection> graph, Heuristic heuristic )
         {
             m_graph = graph;
@@ -24,6 +44,14 @@ namespace Atlas
             m_openList = new IndexedPriorityQueue<NodeRecord>( graph.Nodes.Count );
         }
 
+        /// <summary>
+        /// Finds a path between the given nodes
+        /// </summary>
+        /// <param name="startNodeID">The node to start the path search from</param>
+        /// <param name="endNodeID">The desired goal node</param>
+        /// <param name="resultPath">The shortest path between the given nodes, if successful</param>
+        /// <returns>Result of the search</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the provided <paramref name="resultPath"/> list is null</exception>
         public Result Search( int startNodeID, int endNodeID, List<TConnection> resultPath )
         {
             if ( resultPath == null )

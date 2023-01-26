c43b4dbc39b343669e98e75c68d353c57e409e89
David Knopp <daknopp@gmail.com>
2023-01-25 21:13:46 -0800

    Added quick-select and play/pause buttons to time scale window

--- Assets/Scripts/Editor/Debug/TimeScaleDebugWindow.cs
+++ Assets/Scripts/Editor/Debug/TimeScaleDebugWindow.cs
@@ -12,8 +12,12 @@ namespace Atlas
             window.Show();
         }
 
+        private static float m_previousTimeScale = 1.0f;
+
         private void OnGUI()
         {
+            EditorGUILayout.Space();
+
             using ( var changeCheck = new EditorGUI.ChangeCheckScope() )
             {
                 float timeScale = EditorGUILayout.Slider( "Time Scale", Time.timeScale, 0f, 10f );
@@ -24,15 +28,47 @@ namespace Atlas
                 }
             }
 
-            if ( GUILayout.Button( "Reset" ) )
+            void DrawTimeScaleButton( float timeScale )
             {
-                Time.timeScale = 1.0f;
+                if ( GUILayout.Button( $"{timeScale}x" ) )
+                {
+                    Time.timeScale = timeScale;
+                }
             }
 
-            if ( GUILayout.Button( "Pause" ) )
+            EditorGUILayoutUtility.HorizontalLine();
+
+            EditorGUILayout.BeginHorizontal();
+
+            // pause/play
+            if ( Time.timeScale == 0.0f )
             {
-                Time.timeScale = 0.0f;
+                using ( new GUIBackgroundColorScope( Color.green ) )
+                {
+                    if ( GUILayout.Button( EditorGUIUtility.IconContent( "d_PlayButton" ) ) )
+                    {
+                        Time.timeScale = m_previousTimeScale;
+                    } 
+                }
             }
+            else
+            {
+                using ( new GUIBackgroundColorScope( Color.red ) )
+                {
+                    if ( GUILayout.Button( EditorGUIUtility.IconContent( "d_PauseButton" ) ) )
+                {
+                    m_previousTimeScale = Time.timeScale;
+                    Time.timeScale = 0.0f;
+                    }
+                }
+            }
+
+            DrawTimeScaleButton( 1.0f );
+            DrawTimeScaleButton( 2.0f );
+            DrawTimeScaleButton( 4.0f );
+            DrawTimeScaleButton( 10.0f );
+
+            EditorGUILayout.EndHorizontal();
         }
     }
 }

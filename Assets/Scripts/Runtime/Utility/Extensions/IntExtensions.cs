4e7581ef9989e5ff8cc951c44fc9e9cc1f4268ba
David Knopp <daknopp@gmail.com>
2023-01-29 15:59:26 -0800

    Added extensions for determing if an int is even or odd

---
+++ Assets/Scripts/Runtime/Utility/Extensions/IntExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Atlas
+{
+
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Determines if the given integer is an even number (divisible by 2)
+        /// </summary>
+        /// <param name="value">The integer to check if even</param>
+        /// <returns>True if the value is even, false otherwise</returns>
+        public static bool IsEven( this int value )
+        {
+            return value % 2 == 0;
+        }
+
+        /// <summary>
+        /// Determines if the given integer is an odd number
+        /// </summary>
+        /// <param name="value">The integer to check if odd</param>
+        /// <returns>True if the value is odd, false otherwise</returns>
+        public static bool IsOdd( this int value )
+        {
+            return !value.IsEven();
+        }
+    }
+}
